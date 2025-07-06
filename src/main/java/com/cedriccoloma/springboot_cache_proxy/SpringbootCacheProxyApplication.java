9f60ef5e434c0a52c46eadaa4c176dc745417979
codedbyced <codedbyced@gmail.com>
2025-07-06 19:50:21 +0800

    Add initial codes generated by Spring Initialzr

---
+++ src/main/java/com/cedriccoloma/springboot_cache_proxy/SpringbootCacheProxyApplication.java
@@ -0,0 +1,13 @@
+package com.cedriccoloma.springboot_cache_proxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCacheProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCacheProxyApplication.class, args);
+	}
+
+}
